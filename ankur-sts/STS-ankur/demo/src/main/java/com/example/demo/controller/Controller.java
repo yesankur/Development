5f536931570305481e27a733211388aa32079895
yesankur <ankur.jvm@gmail.com>
2021-07-27 10:14:13 +0530

    api-gatewayFallBack

---
+++ ankur-sts/STS-ankur/demo/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
+public class Controller {
+
+    @GetMapping("/today")
+    public String getMessage() {
+        return "It's a bright sunny day today!";
+    }
+
+}
